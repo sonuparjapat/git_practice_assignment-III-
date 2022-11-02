7c5b04922f3058f83f057c743851854c75cc962f
sonuparjapat <sahil992001parjapat@gmail.com>
2022-11-02 14:00:23 +0530

    My second commit

--- prime.js
+++ prime.js
@@ -1,8 +1,8 @@
 function checkprime(num){
-let count=0;
+let factor=0;
 for (i=1;i<=num;i++){if (num%i==0){
-count=count+1;}}
-if (count==2){return true;}else {return false;}
+factor=factor+1;}}
+if (factor==2){return true;}else {return false;}
 }
 let ans=checkprime(7);
 if (ans==true){console.log("Not prime")}
