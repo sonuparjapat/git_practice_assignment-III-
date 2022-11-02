bc4f5613ded63057d563e665fb5930a070bd29e3
Ravi Pal <ravistarpal@gmail.com>
2022-11-02 13:29:16 +0530

    prime code fixed

--- prime.js
+++ prime.js
@@ -1,9 +1,19 @@
 function checkprime(num){
 let count=0;
-for (i=1;i<=num;i++){if (num%i==0){
-count=count+1;}}
-if (count==2){return true;}else {return false;}
+for (i=1; i<=num; i++){
+    if (num%i==0){
+     count=count++; 
+    }
+  }
+  if (count==2){
+      return true;
+  } 
+   return false;
 }
+
 let ans=checkprime(7);
-if (ans==true){console.log("Not prime")}
-else {console.log("Prime");}
+if (ans==true){
+      console.log("prime")
+}else {
+       console.log("Not Prime");
+}
